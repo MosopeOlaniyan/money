4279c7db9a9714d1df3ac15e71eaf42f35975478
Mosope <moshiola14@gmail.com>
2024-07-30 11:11:16 +0100

    Initial Commit

---
+++ money/moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyApp.swift
+//  money
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
